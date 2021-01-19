8f7f9abc80e450ebcf27b68c8aded975662923fd
Yurii128 <ooii.iixx@mail.ru>
2021-01-19 22:31:18 +0300

    готово

---
+++ three.py
@@ -0,0 +1,9 @@
+number = int(input('Введите число, я с ним сделаю безумно полезное действие: '))
+transformation = str(number)
+number1 = transformation + transformation
+number2 = number1 + transformation
+transformation = int(transformation)
+number1 = int(number1)
+number2 = int(number2)
+number = int(number)
+print(number + number1 + number2)
